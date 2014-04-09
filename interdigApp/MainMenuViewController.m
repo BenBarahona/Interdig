101d83e61265f6ba31386f4c85abb4cc434089c8
BenBarahona <bbaraona@gmail.com>
2014-04-08 22:54:30 -0600

    first commit

--- interdigApp/MainMenuViewController.m
+++ interdigApp/MainMenuViewController.m
@@ -15,7 +15,7 @@
 #import "InputDataViewController.h"
 #import "MapLocationViewController.h"
 
-#define DB_NAME @"interdig"
+#define DB_NAME @"canada1"
 
 @implementation MainMenuViewController
 @synthesize thisObjectInfo, objectArray, objManager, searchResults, urlString, dataBase, request;
